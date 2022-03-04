fa35e4e6f20332e0db35d6e471940c59f2efafe5
masoudborzouie <masoudborzouie@gmail.com>
2022-03-04 13:28:06 +0330

    footer

--- src/Layout/Footer/index.js
+++ src/Layout/Footer/index.js
@@ -4,8 +4,14 @@ export default function Footer() {
   return (
     <div className="p4 m-4 h6">
       <h3>FOOTER</h3>
-      <p> with theinsiden-root public URL.
-      Learn how to configu</p>
+      <p> with theinsiden-root public URL. Learn how to configu</p>
+      <p>
+        with theinsiden-root public URL. Learn how to confwith theinsiden-root
+        public URL. Learn how to confwith theinsiden-root public URL. Learn how
+        to confwith theinsiden-root public URL. Learn how to confwith
+        theinsiden-root public URL. Learn how to confwith theinsiden-root public
+        URL. Learn how to conf
+      </p>
     </div>
   );
 }
