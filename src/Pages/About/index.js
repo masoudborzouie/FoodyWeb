5ad743297a742e267d8bfac04da200a49ffbcac4
masoudborzouie <masoudborzouie@gmail.com>
2022-03-08 11:07:36 +0330

    genau

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -5,11 +5,16 @@ export default function About() {
     <div>
       <h3>About Us</h3>
       <h4>dfgfgfg</h4>
-      <p>fdggNotice the use of %PUBLIC_URL% in the tags above.
-      It will be replaced with the URL of the `public` folder during the build.
-      Only files inside the `public` folder can be referenced from the HTML.
-ith client-side routing and a non-root public URL.
-      Learn how to configure a non-root public URL by running `npm run build`</p>
+      <p>
+        fdggNotice the use of %PUBLIC_URL% in the tags above. It will be
+        replaced withIt will be replaced withIt will be replaced withIt will be
+        replaced withIt will be replaced withIt will be replaced withIt will be
+        replaced withgit add the URLn build`
+      </p>
+      <p>
+        dggNotice the use of %PUBLIC_URL% in the tags above. It will be replaced
+        withIt will be replaced withIt will be replaced withIt will be replaced{" "}
+      </p>
     </div>
   );
 }
