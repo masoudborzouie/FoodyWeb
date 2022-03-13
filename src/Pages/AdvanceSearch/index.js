760657ca3eb141bd34b1e3b27715a7491643fc77
masoudborzouie <masoudborzouie@gmail.com>
2022-03-13 09:17:04 +0330

    004

--- src/Pages/AdvanceSearch/index.js
+++ src/Pages/AdvanceSearch/index.js
@@ -3,3 +3,7 @@ import React from "react";
 export default function AdvanceSearch() {
   return <div></div>;
 }
+<p>
+  dggNotice the use of %PUBLIC_URL% in the tags above. It will be replaced
+  withIt will be replaced withIt will be replaced withIt will be replaced
+</p>;
